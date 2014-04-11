c4017d57981938d74434e4971bc06a11614f0648
Sean Yeh <sean.yeh@gmx.com>
2014-04-11 11:35:29 -0400

    #6

---
+++ 06.hs
@@ -0,0 +1,8 @@
+-- Find out whether a list is a palindrome. A palindrome can be read forward or
+-- backward; e.g. (x a m a x). 
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = case xs of
+    []      -> True
+    [x]     -> True
+    (x:xs)  -> x == (last xs) && isPalindrome (init xs)
